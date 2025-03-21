ae5e84a272b13566f57db55f43a84809c9d6c5c1
RohanSingh <121742846+Roahn333singh@users.noreply.github.com>
2025-03-22 03:05:29 +0530

    Experiment tracking with dvclive done

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -5,6 +5,8 @@ import os
 import pickle
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 import json
+from dvclive import Live
+import yaml
 
 log_dir='logs'
 os.makedirs(log_dir,exist_ok=True)
@@ -26,6 +28,29 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+
+'''Adding yaml config code'''
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
+
+
+
 
 def load_model(model_path:str):
     '''Load the trained model from a file'''
@@ -75,7 +100,7 @@ def eval_model(clf, X_test: np.ndarray, y_test: np.ndarray) -> dict:
         }
 
         logger.debug("Model Evaluation completed !!")
-        return metrics
+        return metrics,y_pred
     except Exception as e:
         logger.error("Error during model Evaluation %s", e)
         raise
@@ -97,12 +122,27 @@ def save_metrics(metrics:dict,file_path:str)->None:
 def main():
 
     try:
+        params=load_params(params_path='params.yaml')
         clf=load_model('./models/model.pkl')
         df=load_data('./data/processed/test_tf_idf.csv')
         X_test=df.iloc[:,:-1].values
         y_test=df.iloc[:,-1].values
+        
 
-        model_eval=eval_model(clf=clf,X_test=X_test,y_test=y_test)
+        model_eval,y_pred=eval_model(clf=clf,X_test=X_test,y_test=y_test)
+
+        
+#-----------------------------------------------------------------------------
+        '''Experiments Tracking with dvclive'''
+
+        with Live(save_dvc_exp=True) as live:
+            live.log_metric('accuracy', accuracy_score(y_test, y_test))
+            live.log_metric('precision', precision_score(y_test, y_pred, average='weighted'))
+            live.log_metric('recall', recall_score(y_test, y_pred, average='weighted') )
+            live.log_metric('f1_score',f1_score(y_test, y_pred, average='weighted'))
+
+            live.log_params(params)
+#-----------------------------------------------------------------------------
 
         save_metrics(model_eval,'reports/metrics.json')
 
